e54a1323f137ff59e5c76cab9b721a39950fab74
lisboaa <douglas.lisboa@edu.unipar.br>
2020-01-15 21:57:39 -0300

    Implementado formulario com informações do git.

--- FRONT-END/web/src/App.js
+++ FRONT-END/web/src/App.js
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
+import "./global.css";
+import "./App.css";
+/*
+Componente: Bloco isolado de HTML, CSS e JS, o qual não intefere no funciona
+no restante da aplicação.
 
-import Header from "./Header";
+Propriedade: Informações que um componente PAI passa para o componente FILHO
 
+Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+*/
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Increment</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form action="" className="">
+          <div className="input-block">
+            <label htmlFor="">Usuário do GiHub</label>
+            <input name="github_username" id="username_github" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
   );
 }
 
