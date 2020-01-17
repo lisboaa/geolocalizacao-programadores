313692b5c889e39d825371057a7f1e834496340c
lisboaa <douglas.lisboa@edu.unipar.br>
2020-01-17 02:26:24 -0300

    Design final da view, criação de componentes, inplementado conexão com back-end.

--- FRONT-END/web/src/App.js
+++ FRONT-END/web/src/App.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
+import "./Main.css";
+
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
 /*
 Componente: Bloco isolado de HTML, CSS e JS, o qual não intefere no funciona
 no restante da aplicação.
@@ -11,96 +17,35 @@ Propriedade: Informações que um componente PAI passa para o componente FILHO
 Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 */
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form action="" className="">
-          <div className="input-block">
-            <label htmlFor="">Usuário do GiHub</label>
-            <input name="github_username" id="username_github" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
+
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/36475975?s=460&v=4"
-                alt="Douglas Fernando"
-              />
-              <div className="user-info">
-                <strong>Douglas Fernando</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Junior. Entusiasta de T.I.</p>
-            <a href="https://github.com/lisboaa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/36475975?s=460&v=4"
-                alt="Douglas Fernando"
-              />
-              <div className="user-info">
-                <strong>Douglas Fernando</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Junior. Entusiasta de T.I.</p>
-            <a href="https://github.com/lisboaa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/36475975?s=460&v=4"
-                alt="Douglas Fernando"
-              />
-              <div className="user-info">
-                <strong>Douglas Fernando</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Junior. Entusiasta de T.I.</p>
-            <a href="https://github.com/lisboaa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/36475975?s=460&v=4"
-                alt="Douglas Fernando"
-              />
-              <div className="user-info">
-                <strong>Douglas Fernando</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Junior. Entusiasta de T.I.</p>
-            <a href="https://github.com/lisboaa">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
